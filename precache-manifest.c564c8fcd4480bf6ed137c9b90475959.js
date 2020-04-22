263d10d5b26c288db2a228e6f67d4bddc7422c83
LanguageXange <hlin0017@seattlecentral.edu>
2020-04-22 18:08:27 +0800

    Updates

---
+++ precache-manifest.c564c8fcd4480bf6ed137c9b90475959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c8707311ad862ae970c5e4a38ba287",
+    "url": "/photosearchapp/index.html"
+  },
+  {
+    "revision": "4d218ce71ccf311d63e6",
+    "url": "/photosearchapp/static/css/main.a0a519e6.chunk.css"
+  },
+  {
+    "revision": "290d665ec5ddaf7f16f5",
+    "url": "/photosearchapp/static/js/2.7a26edbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/photosearchapp/static/js/2.7a26edbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d218ce71ccf311d63e6",
+    "url": "/photosearchapp/static/js/main.76f8ad89.chunk.js"
+  },
+  {
+    "revision": "fddb9080efb7993715b5",
+    "url": "/photosearchapp/static/js/runtime-main.d241222c.js"
+  }
+]);
